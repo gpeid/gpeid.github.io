c11ba7ef9dcf8b274998901bb1cb1c89fa4a8e10
George <ghdiep@gmail.com>
2021-02-25 20:11:32 -0500

    update css, use css modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import './App.module.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<code>
+				<a
+					className="App-link"
+					href="https://www.linkedin.com/in/ghdiep/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					LinkedIn
+				</a>
+			</code>
+			<code>
+				<a href="/triforce">Triforce</a>
+			</code>
+			<code>
+				<a href="/moco-liquor-license">MoCo Liquor License (old data)</a>
+			</code>
+			<Footer />
 		</div>
 	);
 }
