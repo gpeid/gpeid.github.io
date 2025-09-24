a18ce2a38c225e81241a89b4cc0a3d5f9347bdf0
George Diep <ghdiep@gmail.com>
2025-09-24 03:57:34 -0400

    fix ts errors

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,7 @@
 
-const Layout = ({ children }) => {
+import type { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
 	return <div className="container mx-auto">{children}</div>;
 };
 
