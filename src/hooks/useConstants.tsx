7931c89373168d438ddbacb9841aa8ac97a44049
George Diep <ghdiep@gmail.com>
2025-10-20 14:41:38 -0400

    set up context

--- src/hooks/useConstants.tsx
+++ src/hooks/useConstants.tsx
@@ -3,14 +3,14 @@ const useContants = () => {
 
 
     const profileLinks = [
-        { name: 'GitHub', url: 'https://github.com/gpeid' },
-        { name: 'LinkedIn', url: 'https://www.linkedin.com/in/ghdiep/' },
+        { name: 'GitHub', url: 'https://github.com/gpeid', description: "Github Profile" },
+        { name: 'LinkedIn', url: 'https://www.linkedin.com/in/ghdiep/', description: "LinkedIn Profile" },
 
     ];
 
     const portfolioLinks = [
-        { name: 'Rabbit Rundown', url: 'https://rabbit-rundown.vercel.app/' },
-        { name: 'Resume', url: '/resume' },
+        { name: 'Rabbit Rundown', url: 'https://rabbit-rundown.vercel.app/', description: "Fitness performance tracker with data comparison and visualization. React, TypeScript, Supabase, Strava API" },
+        { name: 'Resume', url: '/resume', description: "Outdated resume." },
     ];
 
     return {
